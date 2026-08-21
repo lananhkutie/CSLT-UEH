c7e0beb5b966932912dfff7112862c0edcbbb815
lananhkutie <lananhphamj21052gmail.com>
2026-08-21 19:45:26 +0700

    updated21/8

--- CSLT-UEH/session03/excercise_02.cs
+++ CSLT-UEH/session03/excercise_02.cs
@@ -7,7 +7,7 @@ namespace CSLT_UEH.session02
 {
     internal class Exercises_02
     {
-        static void bai1()
+        static void bai1_slide()
         {//to convert from degrees Celsius to Kelvin and Fahrenheit
             Console.WriteLine("nhap nhiet do C:");
             float celcius;
@@ -27,9 +27,69 @@ namespace CSLT_UEH.session02
                 }
             }
         }
+        static void bai2_slide()
+        {   //Create a program in C# for calculate the surface and volume of a sphere, given its radius.
+            Console.WriteLine("Nhap ban kinh hinh cau:");
+            double radius;
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (double.TryParse(input, out radius)) ;
+                {
+                    Console.WriteLine($"The tich hinh cau la: {radius * radius * radius * 4 / 3 * Math.PI}");
+                    break;
+                }
+                Console.WriteLine("sai dinh dang. Vui long nhap lai:");
+            }
+        }
+        static void bai3_slide()
+        {
+            //Write a program in C# that calculates the result of adding, subtracting, multiplying and dividing two numbers entered by the user.
+            double num1, num2;
+            while (true)
+            {
+                Console.WriteLine("Nhap so thu nhat:");
+                string input1 = Console.ReadLine();
+                if (double.TryParse(input1, out num1))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Du lieu khong hop le. Vui long nhap lai:");
+                }
+            }
+            while (true)
+            {
+                Console.WriteLine("Nhap so thu hai:");
+                string input2 = Console.ReadLine();
+                if (double.TryParse(input2, out num2) && num2 != 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Du lieu khong hop le. Vui long nhap lai:");
+                }
+            }
+            Console.WriteLine($"Tong cua hai so la: {num1 + num2}");
+            Console.WriteLine($"Hieu cua hai so la: {num1 - num2}");
+            Console.WriteLine($"Tich cua hai so la: {num1 * num2}");
+            
+            Console.WriteLine("\nNhan phim bat ky de thoat...");
+            Console.ReadKey();
+        }
         private static void Main(string[] args)
         {
-            bai1 ();
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //bai1_slide();
+            //bai2_slide();
+            //bai3_slide();
+            decimal csd_cu;
+            decimal csd_moi;
+            Console.WriteLine("nhap chi so dien cu");
+
         }
     }
 }
